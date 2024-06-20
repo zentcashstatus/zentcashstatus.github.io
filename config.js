595ef3a6f93337b246d09d73eae0a1d5eb889774
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2024-06-20 21:06:05 +0200

    init

---
+++ config.js
@@ -0,0 +1,7 @@
+var api = 'https://superblockchain.con-ip.com/api/ztc';
+var donationAddress = "";
+var blockTargetInterval = 60;
+var coinUnits = 100;
+var totalSupply =  750000000000;
+var symbol = 'ZTC';
+var refreshDelay = 30000;
